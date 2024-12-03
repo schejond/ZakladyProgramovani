f135391c889fd506aea6fbc92f300d8e0f3247c2
Ondrej Schejbal <oschejbal@microsoft.com>
2024-12-03 22:01:27 +0100

    Add lecture 7 code

---
+++ Lekce7/Person.cs
@@ -0,0 +1,71 @@
+﻿namespace Lekce7
+{
+    // poradi vysvetleni: property, method, constructor, parametrized constructor
+    //      encapsulation (zapouzdreni) - public, private
+    //          gettery, settery
+    //      ukazat vyznam static
+
+    public class Person
+    {
+        private string _name;
+        private string _surename;
+        private DateTime _birthday;
+
+        // toto je defaultni konstruktor
+        // kazda classa (trida) ma defaultni konstruktor POKUD neni definovan custom (vlastni) konstruktor
+        public Person()
+        {
+        }
+
+        // parametrizovany konstruktor
+        public Person(string name, DateTime birthday)
+        {
+            _name = name;
+            _birthday = birthday;
+        }
+
+        // priklad metody
+        public void Talk(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static string GetClassName()
+        {
+            return "Person";
+        }
+
+        // diky teto definici je name read-only
+        public string GetName()
+        {
+            return _name;
+        }
+
+        // metoda na zmenu hodnoty private property
+        private void SetName(string name)
+        {
+            _name = name;
+        }
+
+        public void SetSurename(string surename)
+        {
+            _surename = surename;
+        }
+
+        public string GetSurename()
+        {
+            return _surename;
+        }
+
+        public string GetFullName()
+        {
+            return $"{_name} {_surename}";
+        }
+
+        // metoda na ziskani veku, kombinuje vice promennych z classy
+        public int GetAge()
+        {
+            return (int)(DateTime.Now - _birthday).TotalDays / 365;
+        }
+    }
+}
---
+++ Lekce7/Program.cs
@@ -0,0 +1,38 @@
+﻿// vysvetlit, co to je namespace
+namespace Lekce7
+{
+    // ukazat si, ze i vstupni bod naseho programu je vlastne trida
+    // koncovky pro soubory s tridami jsou .cs
+
+    /*
+     * internal vs. private vysvetleni:
+     * internal je pro assembly scope (pouze pristupne z kodu ve stejnem .exe or .dll souboru (vystupni soubory po kompilaci))
+     * private je pro class scope (pristup pouze z kodu stejne tridy (classy))
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // zacneme postupnou definici tridy Person
+
+            // vytvoreni objektu pomoci prazdneho konstruktoru
+            var person = new Person();
+
+            // pomoci parametrizovaneho konstruktoru
+            var person1 = new Person("Jan", new DateTime(1998, 6, 16));
+            
+            // zavolani metody ("chovani") objektu
+            person1.Talk("Ahoj!");
+
+            // uprava private property pomoci metody
+            person1.SetSurename("Novak");
+
+            var person2 = new Person();
+
+            Console.WriteLine($"{person1.GetFullName()} is {person1.GetAge()}");
+            Console.WriteLine(person2.GetFullName());
+
+            Console.WriteLine(Person.GetClassName());
+        }
+    }
+}
